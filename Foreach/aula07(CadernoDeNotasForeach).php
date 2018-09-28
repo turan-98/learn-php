4ce0360af8347db3903e9d3a0d725823a559942d
Sammy <35262710+sammy08@users.noreply.github.com>
2018-09-28 17:13:20 -0300

    Update aula07(CadernoDeNotasForeach).php

--- Foreach/aula07(CadernoDeNotasForeach).php
+++ Foreach/aula07(CadernoDeNotasForeach).php
@@ -4,7 +4,6 @@
 <title>Notas e Alunos</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width,initial-scale=1.0">
-<link href="main.css" rel="stylesheet">
 <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" media="screen">
 </head>
 <body>
