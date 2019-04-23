38d5492c9c7f6ae7311c19a0df50fc59d16d9347
SamucaSantana <35262710+samucasantana@users.noreply.github.com>
2019-04-23 11:30:00 +0000

    Refatorando código

--- oop/classe.php
+++ oop/classe.php
@@ -5,74 +5,78 @@ class aluno {
 	public $email;
 	public $curso;
 	public $horario;
-	function aluno() {
-		$this->recebeAluno();
-		$this->recebeCurso();
-		$this->recebeHorario();
-	}
+		function __construct(){
+			function recebeAluno() {
+			$this->nome = $_POST['nome'];
+			$this->email = $_POST['email'];
+				$nome_aluno = addslashes($this->nome);
+				$email_aluno = addslashes($this->email);
+	
+				function recebeCurso() {
+					$cursoMateria = $_POST['selectCurso'];
+					switch ($cursoMateria) {
+					case 'Design Responsivo':
+						$cursoMateria = 'Design Responsivo';
+						break;
+					case 'Bootstrap':
+						$cursoMateria = 'Bootstrap';
+						break;
+					case 'Foundation':
+						$cursoMateria = 'Foundation';
+						break;
+					case 'Sass':
+						$cursoMateria = 'Sass';
+						break;
+					case 'JavaScript':
+						$cursoMateria = 'JavaScript';
+						break;
+					case 'Ruby on Rails':
+						$cursoMateria = 'Ruby on Rails';
+						break;
+					case 'PHP':
+						$cursoMateria = 'PHP';
+						break;
+					case 'Big Data':
+						$cursoMateria = 'Big Data';
+						break;
+					case 'Lavarel':
+						$cursoMateria = 'Lavarel';
+						break;
+					case 'Big Data':
+						$cursoMateria = 'Big Data';
+						break;
+					case 'Python':
+						$cursoMateria = 'Python';
+						break;
 
-	function recebeAluno() {
-		$this->nome = $_POST['nome'];
-		$this->email = $_POST['email'];
-
-	}
-
-	function recebeCurso() {
-		$cursoMateria = $_POST['selectCurso'];
-		switch ($cursoMateria) {
-		case 'Design Responsivo':
-			$cursoMateria = 'Design Responsivo';
-			break;
-		case 'Bootstrap':
-			$cursoMateria = 'Bootstrap';
-			break;
-		case 'Foundation':
-			$cursoMateria = 'Foundation';
-			break;
-		case 'Sass':
-			$cursoMateria = 'Sass';
-			break;
-		case 'JavaScript':
-			$cursoMateria = 'JavaScript';
-			break;
-		case 'Ruby on Rails':
-			$cursoMateria = 'Ruby on Rails';
-			break;
-		case 'PHP':
-			$cursoMateria = 'PHP';
-			break;
-		case 'Big Data':
-			$cursoMateria = 'Big Data';
-			break;
-		case 'Lavarel':
-			$cursoMateria = 'Lavarel';
-			break;
-		case 'Big Data':
-			$cursoMateria = 'Big Data';
-			break;
-		case 'Python':
-			$cursoMateria = 'Python';
-			break;
-
-		}
-		$this->curso = $cursoMateria;
-	}
-
-	function recebeHorario() {
-		$horarioCurso = $_POST['turnoCurso'];
-		switch ($horarioCurso) {
-		case 'diurno':
-			$horarioCurso = 'diurno';
-			break;
-		case 'matutino':
-			$horarioCurso = 'matutino';
-			break;
-		case 'noturno':
-			$horarioCurso = 'noturno';
-			break;
+					}
+					$this->curso = $cursoMateria;
+				}
 
 		}
-		$this->horario = $horarioCurso;
+				function recebeHorario() {
+				$horarioCurso = $_POST['turnoCurso'];
+				switch ($horarioCurso) {
+				case 'diurno':
+					$horarioCurso = 'diurno';
+					break;
+				case 'matutino':
+					$horarioCurso = 'matutino';
+					break;
+				case 'noturno':
+					$horarioCurso = 'noturno';
+					break;
+
+				}
+				$this->horario = $horarioCurso;
+			}
+
+	
+
 	}
+
+	
+
+	
 }
 ?>
