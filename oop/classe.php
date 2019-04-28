bfd07a1817320fb2322a2fe8ec3aac54736b029e
Samuel Santana <35262710+turan-98@users.noreply.github.com>
2019-04-28 16:18:48 -0300

    Update

--- oop/classe.php
+++ oop/classe.php
@@ -5,92 +5,85 @@ class aluno {
 	public $email;
 	public $curso;
 	public $horario;
-		function __construct(){
-			
-			function getNome()
-			{
-				return $this->nome = $_POST['nome'];
-			}
-			function getEmail()
-			{
-				return $this->email = $_POST['email'];
-			}
-			function getCurso()
-			{
-				return $cursoMateria = $_POST['selectCurso'];
-			}
-			function getHorario()
-			{
-				return $horarioCurso = $_POST['turnoCurso'];
-			}
-			
-			function setNome()
-			{
-				$this->nome = $nome;
-			}
-			
-			function setEmail()
-			{
-				$this->email = $email;
-			}
-			
-			function setCurso()
-			{
-				switch ($cursoMateria) {
-						
-					case 'Design Responsivo':
-						$cursoMateria = 'Design Responsivo';
-						break;
-					case 'Bootstrap':
-						$cursoMateria = 'Bootstrap';
-						break;
-					case 'Foundation':
-						$cursoMateria = 'Foundation';
-						break;
-					case 'Sass':
-						$cursoMateria = 'Sass';
-						break;
-					case 'JavaScript':
-						$cursoMateria = 'JavaScript';
-						break;
-					case 'Ruby on Rails':
-						$cursoMateria = 'Ruby on Rails';
-						break;
-					case 'PHP':
-						$cursoMateria = 'PHP';
-						break;
-					case 'Big Data':
-						$cursoMateria = 'Big Data';
-						break;
-					case 'Lavarel':
-						$cursoMateria = 'Lavarel';
-						break;
-					case 'Big Data':
-						$cursoMateria = 'Big Data';
-						break;
-					case 'Python':
-						$cursoMateria = 'Python';
-						break;
+	function __construct() {
 
-					}
-				$this->curso = $cursoMateria;
-			}
-				function setHorario() 
-				{
-					switch ($horarioCurso) 
-					{
-					case 'diurno':
-						$horarioCurso = 'diurno';
-						break;
-					case 'matutino':
-						$horarioCurso = 'matutino';
-						break;
-					case 'noturno':
-						$horarioCurso = 'noturno';
-						break;
+		function getNome() {
+			return $this->nome = $_POST['nome'];
+		}
+		function getEmail() {
+			return $this->email = $_POST['email'];
+		}
+		function getCurso() {
+			return $cursoMateria = $_POST['selectCurso'];
+		}
+		function getHorario() {
+			return $horarioCurso = $_POST['turnoCurso'];
+		}
 
-					}
-				$this->horario = $horarioCurso;
+		function setNome() {
+			$this->nome = $nome;
+		}
+
+		function setEmail() {
+			$this->email = $email;
+		}
+
+		function setCurso() {
+			switch ($cursoMateria) {
+
+			case 'Design Responsivo':
+				$cursoMateria = 'Design Responsivo';
+				break;
+			case 'Bootstrap':
+				$cursoMateria = 'Bootstrap';
+				break;
+			case 'Foundation':
+				$cursoMateria = 'Foundation';
+				break;
+			case 'Sass':
+				$cursoMateria = 'Sass';
+				break;
+			case 'JavaScript':
+				$cursoMateria = 'JavaScript';
+				break;
+			case 'Ruby on Rails':
+				$cursoMateria = 'Ruby on Rails';
+				break;
+			case 'PHP':
+				$cursoMateria = 'PHP';
+				break;
+			case 'Big Data':
+				$cursoMateria = 'Big Data';
+				break;
+			case 'Lavarel':
+				$cursoMateria = 'Lavarel';
+				break;
+			case 'Big Data':
+				$cursoMateria = 'Big Data';
+				break;
+			case 'Python':
+				$cursoMateria = 'Python';
+				break;
+
 			}
+			$this->curso = $cursoMateria;
+		}
+		function setHorario() {
+			switch ($horarioCurso) {
+			case 'diurno':
+				$horarioCurso = 'diurno';
+				break;
+			case 'matutino':
+				$horarioCurso = 'matutino';
+				break;
+			case 'noturno':
+				$horarioCurso = 'noturno';
+				break;
+
+			}
+			$this->horario = $horarioCurso;
+		}
+	}
 }
 ?>
+
