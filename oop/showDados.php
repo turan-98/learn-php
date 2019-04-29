fab3b3690d79830d05473b2a92d05a653adaf46d
Samuel Santana <35262710+turan-98@users.noreply.github.com>
2019-04-29 16:39:33 -0300

    Update showDados.php

--- oop/showDados.php
+++ oop/showDados.php
@@ -17,9 +17,9 @@ $aluno = new aluno();
 		<div class="card-body">
 			<h1 class="card-title text-center">Parabéns !!!</h1>
 			<h4>Aluno(a) <?=$aluno->setNome($_POST['nome']);?></h4> <p>esta matriculado no curso </p>
-			<h4><?=$aluno->setcurso($_POST['selectCurso'];);?></h4>
+			<h4><?=$aluno->setCurso($_POST['selectCurso']);?></h4>
 			<p>No periodo </p>
-			<h4><?=$aluno->sethorario($_POST['horario']);?></h4>
+			<h4><?=$aluno->setHorario($_POST['turnoCurso']);?></h4>
 	</div>
 </div>
 </body>
